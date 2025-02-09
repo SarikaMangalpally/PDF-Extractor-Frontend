488c0617218b5abc8d6a4c6c04e73407dc1aa2e6
Sarika Mangalpally <39065053+SarikaMangalpally@users.noreply.github.com>
2025-02-09 14:24:46 -0600

    spin loader added

--- app/store/usePdfStore.tsx
+++ app/store/usePdfStore.tsx
@@ -34,13 +34,17 @@ export interface PdfStore {
   setPdfText: (pdf_text: PdfText[]) => void;
   bbox_overlay: BboxOverlay | null;
   setBboxOverlay: (bbox_overlay: BboxOverlay) => void;
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
 }
 
 export const usePdfStore = create<PdfStore>((set) => ({
   pdf_url: '', // Ensure this is initialized properly
   pdf_text: [],
   bbox_overlay: null,
+  isLoading: false,
   setPdfText: (pdf_text: PdfText[]) => set({ pdf_text }),
   setPdfUrl: (pdf_url: string) => set({ pdf_url }), // Ensure URL updates
   setBboxOverlay: (bbox_overlay: BboxOverlay) => set({ bbox_overlay }),
+  setIsLoading: (isLoading: boolean) => set({ isLoading }),
 }));
