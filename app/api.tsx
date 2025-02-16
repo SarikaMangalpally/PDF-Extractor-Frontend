1e94cf0226186b144d3fc83d647f6b3f0a7d6a35
Sarika Mangalpally <39065053+SarikaMangalpally@users.noreply.github.com>
2025-02-16 00:03:21 -0600

    Update api.tsx

--- app/api.tsx
+++ app/api.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 // const API_URL = 'http://localhost:8000';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -9,7 +11,7 @@ export const uploadURL = (url: string) => {
   if (!url || !url.toLowerCase().endsWith(".pdf")) {
     throw new Error("Invalid URL: must be a valid PDF URL.");
   }
-  return axios.post(`${API_URL}/extract`, { pdf_url: url });
+  return axios.post(`${API_URL}/extract`, { pdf_url: url }, { headers: { 'Content-Type': 'application/json' } });
 };
 
 export const getUrlText = (url: string) => {
